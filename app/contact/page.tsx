b671300adf92cf0beff32d1162b4f14b9c57f147
BlueSi23 <60506733+BlueSi23@users.noreply.github.com>
2025-12-22 01:08:29 +0000

    Add bottom padding to contact page for mobile footer clearance

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,10 +3,9 @@ import { ContactForm } from "../../components/sections/ContactForm";
 export const revalidate = 0;
 
 export default function ContactPage() {
-  return <ContactForm />;
+  return (
+    <div style={{ paddingTop: '3rem', paddingBottom: '8rem' }}>
+      <ContactForm />
+    </div>
+  );
 }
-
-
-
-
-
