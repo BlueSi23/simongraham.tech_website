3a6c27a2a164e4d01756f2c048d023c21539ff00
BlueSi23 <60506733+BlueSi23@users.noreply.github.com>
2026-01-01 16:40:33 +0000

    Fix middleware matcher for admin root

--- middleware.ts
+++ middleware.ts
@@ -34,5 +34,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/admin/:path*", "/api/experiments"],
+    matcher: ["/admin", "/admin/:path*", "/api/experiments"],
 };
